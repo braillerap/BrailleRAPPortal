4cfaf1c3de816e7bbdd00ee691fee8a5c3036a83
Stephane <pub.sg@free.fr>
2026-09-02 03:47:45 +0200

    feat: move Flask secret in environnement file

--- portal.py
+++ portal.py
@@ -1,4 +1,10 @@
+from dotenv import dotenv_values
 from app import app
+print (dotenv_values(".env"))
+
+# config secret for csrf flask protection
+app.config['SECRET_KEY'] = dotenv_values(".env")["SECRET_KEY"]
+
 
 if __name__ == '__main__':
     print ('http://localhost:5000')
